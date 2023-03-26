d1841be6eb2036b0bbff371bec02ba46ef01cb1e
Maria Fernanda Leon Montoya <124616296+Marifer206@users.noreply.github.com>
2023-03-25 19:20:25 -0500

    Add files via upload

---
+++ EJERCICIO #1 RETO 7.py
@@ -0,0 +1,6 @@
+x:int = 25 #<inicia>
+print('El valor inicial de x es '+ str(x))
+while(x <= 24): #<cond>
+  print('El valor de x durante el ciclo') #<bloque>
+  x = x + 1 #<actualiza> 
+print('El valor final de x es '+ str(x))
